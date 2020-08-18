1305e09c5847fbed52389a0842304948f577b617
Kelvin Kiptum Kiprop <kelvin.kiprop96@gmail.com>
2020-08-19 01:50:12 +0300

    - added base html structure for a few sections

--- SiteAssets/js/index.js
+++ SiteAssets/js/index.js
@@ -1,4 +1,9 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.dropdown-trigger');
-    var instances = M.Dropdown.init(elems, null);
+document.addEventListener('DOMContentLoaded', function () {
+    var dropdownElem = document.querySelectorAll('.dropdown-trigger');
+    var instances = M.Dropdown.init(dropdownElem, null);
+
+    var carouselElem = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(carouselElem, {
+        indicators: true
+    });
 });
