d9ff38566cf35702c1dd466f5a705b582673f2f6
Kelvin Kiptum Kiprop <kelvin.kiprop96@gmail.com>
2020-08-20 21:21:11 +0300

    - RWD design complete

--- SiteAssets/js/index.js
+++ SiteAssets/js/index.js
@@ -1,9 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var dropdownElem = document.querySelectorAll('.dropdown-trigger');
-    var instances = M.Dropdown.init(dropdownElem, null);
+	var dropdownElem = document.querySelectorAll('.dropdown-trigger');
+	var instances = M.Dropdown.init(dropdownElem, null);
 
-    var carouselElem = document.querySelectorAll('.carousel');
-    var instances = M.Carousel.init(carouselElem, {
-        indicators: true
-    });
+	var carouselElem = document.querySelectorAll('.carousel');
+	var instances = M.Carousel.init(carouselElem, {
+		indicators: true
+	});
+
+	var sideNavElem = document.querySelectorAll('.sidenav');
+	var instances = M.Sidenav.init(sideNavElem, null);
+
+	var collapsibleElem = document.querySelectorAll('.collapsible');
+	var instances = M.Collapsible.init(collapsibleElem, null);
 });
